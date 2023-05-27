b50e41df1c23f23995ca7f2435da155abd7fd514
Jonathan Moregård <jonathan.moregard@carboncloud.com>
2023-05-27 15:42:59 +0200

    got it working!

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
-import OpenAI.Client
-import OpenAI.Resources
+
+import Data.Bifunctor (second)
+import Data.Either (Either, either)
+import Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Vector as V
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
-import System.Environment (getEnv)
-import qualified Data.Vector as V
-import Data.Either (Either, either)
+import OpenAI.Client
+import OpenAI.Resources
+import System.Environment (getEnv, lookupEnv)
 
 main :: IO ()
 main = do
   manager <- newManager tlsManagerSettings
-  maybeApiKey <- lookupEnv "OPENAI_KEY"
-  case maybeApiKey of
-    Nothing -> putStrLn "Could not find OPENAI_KEY in environment variables."
+  configKvp <- fmap (second (T.replace "=" "") . T.breakOn "=") . T.lines <$> TIO.readFile "app.config" -- get config key value pairs
+  case lookup "OpenAiSecret" configKvp of
+    Nothing -> putStrLn "Could not find OPENAI_KEY in app.config."
     Just apiKey -> do
-      let client = makeOpenAIClient (T.pack apiKey) manager 4
-      let engineId = EngineId "text-davinci-002"  -- EngineId may vary, check OpenAI API docs
-      let prompt = defaultTextCompletionCreate "Translate the following English text to French: '{text}'"
+      let client = makeOpenAIClient apiKey manager 4
+      let engineId = EngineId "text-davinci-002" -- EngineId may vary, check OpenAI API docs
+      let prompt = defaultTextCompletionCreate "Translate the following English text to French: 'Hello, how are you?'"
       result <- completeText client engineId prompt
       case result of
-        Left err -> print err  -- Print the error if the request fails
+        Left err -> print err -- Print the error if the request fails
         Right response -> do
           let choices = tcChoices response
           if V.null choices
