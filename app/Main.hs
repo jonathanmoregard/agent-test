728d2296004a83a82146189dea7a9e8640430384
Jonathan Moregård <jonathan.moregard@carboncloud.com>
2023-05-27 20:36:15 +0200

    start using effectful

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,18 @@
 import Data.Bifunctor (second)
 import Data.Either (Either, either)
 import Data.Text as T
-import qualified Data.Text.IO as TIO
-import qualified Data.Vector as V
-import Network.HTTP.Client
+import Data.Text.IO qualified as TIO
+import Data.Vector qualified as V
+import Effect.LLM (LLMEffect, Prompt (..), Response (..))
+import Effect.LLM qualified as LLME
+import Effectful (Dispatch (..), DispatchOf, Eff, Effect, IOE, MonadIO (liftIO), (:>))
+import Effectful qualified as IOE
+import Effectful.Error.Dynamic (CallStack, Error, prettyCallStack, throwError)
+import Effectful.Error.Dynamic qualified as Error
+import Network.HTTP.Client hiding (Response)
 import Network.HTTP.Client.TLS
 import OpenAI.Client (OpenAIClient)
-import qualified OpenAI.Client as OpenAI
+import OpenAI.Client qualified as OpenAI
 import OpenAI.Resources
 import System.Environment (getEnv, lookupEnv)
 
@@ -16,35 +22,18 @@ main :: IO ()
 main = do
   manager <- newManager tlsManagerSettings
   configKvp <- fmap (second (T.replace "=" "") . T.breakOn "=") . T.lines <$> TIO.readFile "app.config" -- get config key value pairs
-  case lookup "OpenAiSecret" configKvp of
-    Nothing -> putStrLn "Could not find OPENAI_KEY in app.config."
-    Just apiKey -> do
-      let client = OpenAI.makeOpenAIClient apiKey manager 4
-      let engineId = EngineId "text-davinci-002" -- EngineId may vary, check OpenAI API docs
-      let prompt = defaultTextCompletionCreate
-      result <- promptOpenAi client engineId (Prompt "Translate the following English text to French: 'Hello, how are you?'")
-      case result of
-        Left err -> print $ "Error! Info: " <> show err
-        Right response -> print response
+  let engineId = EngineId "text-davinci-002" -- EngineId may vary, check OpenAI API docs
+  IOE.runEff $ do
+    (result :: Either (CallStack, Text) ()) <- Error.runError $ do
+      client <- case lookup "OpenAiSecret" configKvp of
+        Nothing -> throwError $ T.pack "Could not find OPENAI_KEY in app.config."
+        Just apiKey -> pure $ OpenAI.makeOpenAIClient apiKey manager 4
+      LLME.runLLMEffect client engineId program
+    case result of
+      Right () -> pure ()
+      Left (cs, err) -> liftIO $ print $ "Error: " <> show err <> ", callstack: " <> prettyCallStack cs
 
-data CompleteTextError = ClientError OpenAI.ClientError | NoChoicesInResponse
-  deriving (Eq, Show)
-
-newtype Prompt
-  = Prompt Text
-
-promptOpenAi :: OpenAIClient -> EngineId -> Prompt -> IO (Either CompleteTextError Text)
-promptOpenAi client engine (Prompt p) = do
-  result <- OpenAI.completeText client engine $ defaultTextCompletionCreate p
-  case result of
-    Left err -> returnError $ ClientError err -- Print the error if the request fails
-    Right response -> do
-      let choices = tcChoices response
-      if V.null choices
-        then returnError NoChoicesInResponse
-        else do
-          let firstChoice = V.head choices
-          let outputText = tccText firstChoice
-          pure $ Right outputText
-  where
-    returnError = pure . Left
+program :: (IOE :> es, LLMEffect :> es) => Eff es ()
+program = do
+  res <- LLME.completeText (Prompt "Translate the following English text to French: 'Hello, how are you?'")
+  liftIO $ print res
---
+++ src/Effect/LLM.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Effect.LLM (Prompt (..), Response (..), LLMEffect, completeText, runLLMEffect) where
+
+import Control.Exception (IOException)
+import Data.Bifunctor (second)
+import Data.Either (Either, either)
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
+import Data.Vector qualified as V
+import Effectful (Dispatch (..), DispatchOf, Eff, Effect, IOE, MonadIO (liftIO), (:>))
+import Effectful.Dispatch.Dynamic (interpret, send)
+import Effectful.Error.Dynamic (Error, throwError)
+import Network.HTTP.Client qualified as HTTPClient
+import Network.HTTP.Client.TLS qualified as HTTPClient
+import OpenAI.Client (OpenAIClient)
+import OpenAI.Client qualified as OpenAI
+import OpenAI.Resources
+  ( EngineId,
+    TextCompletion (tcChoices),
+    TextCompletionChoice (tccText),
+    defaultTextCompletionCreate,
+  )
+import System.Environment (getEnv, lookupEnv)
+import UnliftIO (catch)
+
+newtype Prompt
+  = Prompt Text
+
+newtype Response
+  = Response Text
+  deriving (Eq, Show)
+
+data LLMEffect :: Effectful.Effect where
+  CompleteText :: Prompt -> LLMEffect m Response
+
+type instance DispatchOf LLMEffect = 'Dynamic
+
+completeText :: LLMEffect :> es => Prompt -> Eff es Response
+completeText prompt = send $ CompleteText prompt
+
+runLLMEffect ::
+  (IOE :> es, Error Text :> es) =>
+  OpenAIClient ->
+  EngineId ->
+  Eff (LLMEffect : es) a ->
+  Eff es a
+runLLMEffect client engine = interpret $ \_ -> \case
+  CompleteText (Prompt prompt) -> do
+    result <- adapt $ OpenAI.completeText client engine $ defaultTextCompletionCreate prompt
+    case result of
+      Left err -> throwError $ "Client error:" <> T.pack (show err)
+      Right response -> do
+        let choices = tcChoices response
+        if V.null choices
+          then throwError $ T.pack "No choices in response"
+          else pure $ Response $ tccText (V.head choices)
+    where
+      adapt :: (IOE :> es, Error Text :> es) => IO a -> Eff es a
+      adapt m = liftIO m `catch` \(e :: IOException) -> throwError $ T.pack (show e)
